3b36e8c76e469b572f0f06d5b431db77b2f9a7e0
chiragpandya88 <chiragpandya88@gmail.com>
2017-05-23 20:54:16 +0530

    Making the address column
    Making dummy variables for product code and company name

--- refine.R
+++ refine.R
@@ -12,7 +12,7 @@ company_clean <- function(x,y) {
   return(y)
 }
 
-sales$company = company_clean("phillips",sales$company)
+sales$company = company_clean("philips",sales$company)
 sales$company = company_clean("akzo",sales$company)
 sales$company = company_clean("van houten",sales$company)
 sales$company = company_clean("unilever",sales$company)
@@ -21,7 +21,19 @@ sales$company = company_clean("unilever",sales$company)
 sales <- separate(sales,"Product.code...number",into = c("product_code","product_number"),sep="-")
 
 #Adding product category
-sales <-mutate(sales,product_cat = ifelse(sales$product_code == "p","smartphone",ifelse(sales$product_code == "x","laptop",ifelse(sales$product_code == "v","tv","tablet"))))
-sales
+sales <-mutate(sales,product_cat = ifelse(product_code == "p","smartphone",ifelse(product_code == "x","laptop",ifelse(product_code == "v","tv","tablet"))))
 
+#Making the address column
+sales <- mutate(sales,full_address=paste(address,city,country,sep = ","))
 
+#Creating Dummy Variables
+sales <- mutate(sales,company_philips = as.numeric((company == "philips")))
+sales <- mutate(sales,company_akzo = as.numeric((company == "akzo")))
+sales <- mutate(sales,company_van = as.numeric((company == "van houten")))
+sales <- mutate(sales,company_unilever = as.numeric((company == "unilever")))
+sales <- mutate(sales,product_smartphone = as.numeric((product_code == "p")))
+sales <- mutate(sales,product_tv = as.numeric((product_code == "v")))
+sales <- mutate(sales,product_laptop = as.numeric((product_code == "x")))
+sales <- mutate(sales,product_tablet = as.numeric((product_code == "q")))
+
+View(sales)
